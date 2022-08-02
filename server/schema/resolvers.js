82edc5f0ef0c69ba28722148f4973b6810617512
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-02 12:42:35 -0700

    added typeDefs

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,4 +1,4 @@
-const { Exercise, Workout } = require('../models');
+const { Exercise, Workout, User } = require('../models');
 
 const resolvers = {
   Query: {
@@ -9,6 +9,10 @@ const resolvers = {
       const params = _id ? { _id } : {};
       return Workout.find(params);
     },
+    user: async (parent,  { _id } ) => {
+      const params = _id ? { _id } : {};
+      return User.find(params)
+    }
   },
   Mutation: {
     createWorkout: async (parent, args) => {
@@ -24,6 +28,26 @@ const resolvers = {
     //   );
     //   return vote;
     // },
+    createUser: async (parent, args) => {
+      const user = await User.create(args, {new : true});
+      return user;
+    },
+    deleteUser: async (parent, { _id }) => {
+      const user = await User.destroy(
+        { _id },
+        { new: true }
+      );
+      return user;
+    },
+    updateUser: async (parent, {_id, workoutName }) => {
+      const user = await User.findOneAndUpdate(
+        { _id },
+        { $inc: { [`workout${workoutName}`] : Exercise} },
+        { new: true}
+      );
+
+      return user;
+    }
   },
 };
 
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,1 +1,26 @@
-//tbd
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+ type User {
+    _id: ID!
+    username: String!
+    email: String!
+    workouts: [Workout]
+ }
+
+ type Query {
+    workout: [Workout]
+    user(username: String): User
+ }
+
+
+ type Mutation {
+    createUser(username: String!, email: String!, password: String!) : Auth
+    deleteUser(_id: String!) : User
+    login(email; String, password: String): Auth
+ }
+
+
+`;
+
+module.exports = typeDefs;
