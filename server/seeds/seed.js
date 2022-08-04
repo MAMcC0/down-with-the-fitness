ce1469a41bf4d53e58b5489b26da6d8d54ae33a1
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-04 12:34:06 -0700

    Seeeds fixed for nowgit add .

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -3,17 +3,40 @@ const db = require('../config/connection');
 const { User, Workout, Exercise } = require('../models')
 const exercises = require('./exercises.json');
 const workouts = require('./workouts.json');
-const users = require('./users.json');
+const usersSeeds = require('./users.json');
 
 db.once('open', async () => {
     await User.deleteMany({})
     await Workout.deleteMany({})
     await Exercise.deleteMany({})
-    await Exercise.insertMany(exercises)
-     await Workout.insertMany(workouts)
-      await User.insertMany(users)
-      console.log("Seeds planted!")
-      process.exit(0);
-     
-    
+    // await Exercise.insertMany(exercises)
+    // await Workout.insertMany(workouts)
+    const users = await User.create(usersSeeds)
+    for (let i = 0; i < workouts.length; i++) {
+        const { _id } = await Workout.create(workouts[i]);      
+        const exerciseSeeds = await User.findOneAndUpdate(
+          { _id: users[Math.floor(Math.random()* users.length)]._id },
+          {
+            $addToSet: {
+                workouts: _id,
+            },
+          }
+        );
+      }
+    for (let i = 0; i < exercises.length; i++) {
+        const { _id, workoutName } = await Exercise.create(exercises[i]);
+        console.log(workoutName);      
+        const exerciseSeeds = await Workout.findOneAndUpdate(
+          { workoutName: workoutName },
+          {
+            $addToSet: {
+                exercises: _id,
+            },
+          }
+        );
+      }
+    console.log("Seeds planted!")
+    process.exit(0);
+
+
 });
