9ab3682709b0ff47f915297c6a172a247496037e
jennabuettner <jenna.e.buettner@gmail.com>
2022-08-04 11:04:04 -0700

    undid exercise list

--- client/src/components/customworkout.js
+++ client/src/components/customworkout.js
@@ -1,6 +1,5 @@
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 export default function customWorkoutsButtons() {
   return (
