4ab58258eb0a657fbf3c60739f44d46850ce388a
jennabuettner <jenna.e.buettner@gmail.com>
2022-08-04 08:42:08 -0700

    added some components

---
+++ client/src/components/customworkoutmodal.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function createModal() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <div class="create-workout-modal">
+            <Button variant="dark" onClick={handleShow}>Create Custom Workout</Button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create Custom Workout</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>FORM**
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="dark" onClick={handleClose}>Create</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
---
+++ client/src/components/exerciselist.js
@@ -0,0 +1,90 @@
+import Button from 'react-bootstrap/Button'
+
+export default function exerciseListButtons() {
+  return (
+
+    <div class="exercise-buttons">
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+        
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+        
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+        
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+        
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+        <Button variant="primary" size="lg" active>Create Custom Workout</Button>{' '}
+
+    </div>
+
+  );
+}
