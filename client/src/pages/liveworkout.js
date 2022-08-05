e120edcd5ac742baafa5d15e77470d0aa17192ec
GSiggins <gregory.siggins@gmail.com>
2022-08-05 13:27:29 -0700

    working on fixing compiling on liveworkout page

--- client/src/pages/liveworkout.js
+++ client/src/pages/liveworkout.js
@@ -5,61 +5,50 @@ import ExerciseTimer from '../components/exercisetimer';
 import React, { useEffect, useState } from 'react'
 
 function LiveWorkout() {
-  
-  let timerDuration = (data) => {
-        
-    let workoutArray = data.workout.exercises;
-    let duration = 0;
-    workoutArray.forEach(exercise => duration = exercise.duration + duration);
-    
-    return duration; 
-};
-    const {data} = useQuery(QUERY_WORKOUTS);
-    let [timerWorkout, setTimer] = useState(timerDuration(data));
-    let [exerciseTime, setExerciseTime] = useState(ExerciseTimer(data));
-    const [currentEx, setCurrentEx] = useState('');
-    let [index, setIndex] = useState(0);
-    const [checkTime, setCheckTime] = useState(false);
+  const { data } = useQuery(QUERY_WORKOUTS);
+  let [timerWorkout, setTimer] = useState(setTimer(data));
+  let [exerciseTime, setExerciseTime] = useState(ExerciseTimer(data));
+  const [currentEx, setCurrentEx] = useState('');
+  let [index, setIndex] = useState(0);
+  const [checkTime, setCheckTime] = useState(false);
 
-    
-    let ExerciseTimer = (data) => {
-        let duration = data.workouts.exercise.duration[index];
-        return duration;
+  let ExerciseTimer = (data) => {
+    let duration = data.workouts.exercise.duration[index];
+    return duration;
+  };
 
-    };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      let descTime = timerWorkout--;
+      let exerciseDescTime = exerciseTime--;
+      setTimer(descTime);
+      setExerciseTime(exerciseDescTime);
+      checkExerciseTime(exerciseTime);
+    }, 1000);
+  }, [timerWorkout]);
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-         let descTime =  timerWorkout-- ;
-         let exerciseDescTime = exerciseTime --;
-          setTimer(descTime);
-          setExerciseTime(exerciseDescTime);
-          checkExerciseTime(exerciseTime);
-        }, 1000);
-      }, [timerWorkout]);
+  const checkExerciseTime = (exTime) => {
+    if (exTime === 0) {
+      return setCheckTime(true);
+    }
+  };
 
-      const checkExerciseTime = (exTime) => {
-        if(exTime === 0){
-          return setCheckTime(true);
-        } 
-      };
+  useEffect(() => {
+    let exName = data.workout.exercises[index].exerciseName;
+    setCurrentEx(exName);
+    let newIndex = index++;
+    setIndex(newIndex);
+  }, [checkTime]);
 
-    useEffect(() => {
-        let exName =  data.workout.exercises[index].exerciseName;  
-        setCurrentEx(exName);
-        let newIndex = index ++;
-        setIndex(newIndex);
-    }, [checkTime]);
+  // useEffect (() => {
 
-    // useEffect (() => {
-   
-    // }, [])
+  // }, [])
 
   return (
     <div>
-   <WorkoutTimer timer={timerWorkout} />
-   <ExerciseTimer exTimer={exerciseTime}/>
-   </div>
+      <WorkoutTimer timer={timerWorkout} />
+      <ExerciseTimer exTimer={exerciseTime} />
+    </div>
   )
 }
 
