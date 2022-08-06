3051da66907d3122f75a4b7d1f2058ceece3720c
GSiggins <gregory.siggins@gmail.com>
2022-08-06 16:07:01 -0700

    fixed functionality of workout list population

--- client/src/pages/workoutpage.js
+++ client/src/pages/workoutpage.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import WorkoutChoices from '../components/workoutchoices'
+import { useQuery } from '@apollo/client';
+import { QUERY_WORKOUTS } from '../utils/queries'
 
+function WorkoutPage() { 
+const { loading, data } = useQuery(QUERY_WORKOUTS);
+const workout = data?.workouts || [];
+console.log(workout)
 
-function WorkoutPage() {
     return (
         <div>
-            <WorkoutChoices />
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <WorkoutChoices 
+                workout={workout}
+                />
+            )
+            }
         </div>
     )
 }
