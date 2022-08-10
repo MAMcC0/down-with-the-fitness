f504fd9d78058879764e44eadb18dcd6c22ec8ca
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-08-09 22:31:04 -0700

    added backend comments

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -6,10 +6,15 @@ const workouts = require('./workouts.json');
 const usersSeeds = require('./users.json');
 
 db.once('open', async () => {
+  //deletes seeds if they exists
     await User.deleteMany({})
     await Workout.deleteMany({})
     await Exercise.deleteMany({})
+    //creates user seeds
     const users = await User.create(usersSeeds)
+
+    //loops through and for each workout creates a workout,
+    //exerciseSeeds are randomized for workouts
     for (let i = 0; i < workouts.length; i++) {
         const { _id } = await Workout.create(workouts[i]);      
         const exerciseSeeds = await User.findOneAndUpdate(
@@ -21,6 +26,7 @@ db.once('open', async () => {
           }
         );
       }
+      //exercises are created and workouts are updated to have those exercieses
     for (let i = 0; i < exercises.length; i++) {
         const { _id, workoutName } = await Exercise.create(exercises[i]);     
         const exerciseSeeds = await Workout.findOneAndUpdate(
